c2db5f5dcff8ec04fd2916cb893ffbcdefb6f65e
Daniel M <danielmiao2019@icloud.com>
2023-04-15 21:22:10 -0700

    update release names

---
+++ generate_next_release.py
@@ -0,0 +1,21 @@
+import subprocess
+import datetime
+
+tags_process = subprocess.Popen(["git", "for-each-ref", "--sort=creatordate", "--format", "'%(refname) %(taggerdate)'", "refs/tags"], stdout=subprocess.PIPE)
+tags = ""
+
+while True:
+	data = tags_process.stdout.read(1)
+	if not data:
+		break
+
+	tags += data.decode("UTF-8")
+
+for tag in reversed(tags.splitlines()):
+	tag = tag[1:-2].replace("refs/tags/", "")
+	if not tag.startswith("v"):
+		continue
+
+	time = datetime.datetime.utcnow().strftime("%Y%m%d%H%M%S")
+	print(f"{tag}+{time}")
+	break
