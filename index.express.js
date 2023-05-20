382e86b40ea9cfb69fa307625ae2eae981794ef4
Daniel M <danielmiao2019@icloud.com>
2023-05-20 10:44:36 -0700

    change routes in index.express.js

--- index.express.js
+++ index.express.js
@@ -22,6 +22,11 @@ app.get("/aaaaaa-test", function(_, response) {
   response.sendFile(path.join(process.cwd(), "index.html"));
 });
 
+app.get("/test-7666", function(_, response) {
+  // eslint-disable-next-line no-undef
+  response.sendFile(path.join(process.cwd(), "index.html"));
+});
+
 app.get("/test-11111111", function(_, response) {
   // eslint-disable-next-line no-undef
   response.sendFile(path.join(process.cwd(), "index.html"));
@@ -34,6 +39,11 @@ app.get("/test-11111112222222222", function(_, response) {
 
 app.get("*", function(_, response) {
   response.status(404).end();
+});
+
+app.get("/test/asdfgh", function(_, response) {
+  // eslint-disable-next-line no-undef
+  response.sendFile(path.join(process.cwd(), "index.html"));
 });
 
 app.get("/test1", async function(request, response) {
